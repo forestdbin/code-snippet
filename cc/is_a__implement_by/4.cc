9c8a02ef9eb084d7a037a0037d9d3c900d60d8eb
forestdbin <forestdb@hotmail.com>
2020-05-08 17:31:03 +1000

    C++ is_a, implement_by

---
+++ cc/is_a__implement_by/4.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+class Animal {
+private:
+    T t;
+public:
+    void talk() {
+        t.talkImplement();
+    }
+};
+
+class SayMiao {
+public:
+    void talkImplement() {
+        cout << "Miao.\n";
+    }
+};
+
+class SayHW {
+public:
+    void talkImplement() {
+        cout << "Hello, world.\n";
+    }
+};
+
+template<typename T>
+void LetAnimalTalk(Animal<T> & pa) {
+    pa.talk();
+}
+
+int main()
+{
+    Animal<SayMiao> am;
+    Animal<SayHW> ah;
+
+    LetAnimalTalk(am);
+    LetAnimalTalk(ah);
+
+    vector<Animal<SayMiao>> va;
+    va.push_back(am);
+    // va.push_back(ah);
+
+    return 0;
+}
