d15fc52c5bb153d4c007cd01fc6b3bfd10a59323
forestdbin <forestdb@hotmail.com>
2021-05-02 10:24:25 -0400

    Add scratch scaffold.

---
+++ scratch/all_3p.h
@@ -0,0 +1,6 @@
+#ifndef _ALL_3P_H_
+#define _ALL_3P_H_
+
+
+
+#endif // _ALL_3P_H_
---
+++ scratch/all_common.h
@@ -0,0 +1,20 @@
+#ifndef _ALL_COMMON_H_
+#define _ALL_COMMON_H_
+
+#ifdef __cplusplus
+// standard C++ library
+
+#include <cstdio>
+
+#else // !__cplusplus
+// standard C library
+
+#include <stdio.h>
+
+#endif // __cplusplus
+
+// common Linux library
+
+#include <unistd.h>
+
+#endif // _ALL_COMMON_H_
